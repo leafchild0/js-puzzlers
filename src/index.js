0d95065bb4b17a09fa250733fe16021a9dcd3fd7
Viktor Malyshev <vmalyshev0@gmail.com>
2023-10-06 12:54:20 +0200

    Magic number

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { findWord } from './twod-words.js'
 import { moveDisks } from './hanoi-towers.js'
+import { findMagicNumber } from './magic-index.js'
 
 const twoDarray = [
     ["a", "b", "c", "d", "e", "f", "g", "h"],
@@ -15,3 +16,6 @@ console.assert(findWord(twoDarray, "dfojdosifj") === false);
 
 
 moveDisks(4, 'a', 'c', 'b')
+
+console.assert(findMagicNumber([-5, -4, -2, 0, 1, 2, 3, 5, 6, 7, 9, 11, 13, 20, 22, 24, 25]) === 11)
+console.assert(findMagicNumber([1, 4, 4, 4, 5, 5, 6, 6, 6, 11, 12, 12, 12, 15, 17, 20, 21]) === 5)
---
+++ src/magic-index.js
@@ -0,0 +1,33 @@
+/**
+ * Magic index
+ *
+ * Problem: Consider a sorted array of n elements that allows duplicates.
+ * An index k is magic if array[k] = k.
+ * Write a recursive algorithm that finds the first magic index
+ */
+
+function findMagicNumber(array) {
+    return findNumber(array, 0, array.length - 1)
+}
+
+function findNumber(array, start, end) {
+    // Return invalid index
+    if (start > end) return -1
+
+    const mid = Math.round((start + end) / 2)
+    const current = array[mid]
+
+    // In case this is the magic number
+    if (current === mid) return mid
+
+    // Search left
+    const left = findNumber(array, start, Math.min(mid - 1, current))
+    if (left >= 0) return left
+
+    // Search right
+    return findNumber(array, Math.max(mid + 1, current), end)
+}
+
+export {
+    findMagicNumber
+}
