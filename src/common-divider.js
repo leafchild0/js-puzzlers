52f4dfa6ca914467f06be9804c0dbda391cfb40a
Viktor Malyshev <vmalyshev0@gmail.com>
2023-10-07 19:54:18 +0200

    gcd description

--- src/common-divider.js
+++ src/common-divider.js
@@ -1,3 +1,11 @@
+/**
+ * Find a common divider of two numbers
+ * Greatest common divider is a biggest number that 2 numbers can be divided to
+ * For example for 9 and 3, biggest divider is 3, but for 12 and 13 it's 1
+ * @param x - first number
+ * @param y - second number
+ * @returns divider number
+ */
 export function gcd(x, y) {
     x = Math.abs(x);
     y = Math.abs(y);
